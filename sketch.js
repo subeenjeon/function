d6403ad7a9ba489020bb09181753abf018b617b4
subeenjeon <powersubeen@naver.com>
2014-10-16 16:21:25 +0900

    f

---
+++ sketch.js
@@ -0,0 +1,35 @@
+function setup(){
+	createCanvas(800, 800);
+}
+
+function draw(){
+	background(0);
+	fill(255);
+	ellipse(mouseX, mouseY, 100, 100);
+
+
+
+//doughnut #2//
+	fill(245, 212, 200);
+	ellipse(400, 400, 200, 200);
+	fill(0);
+	ellipse(400, 400, 130, 130);
+
+//doughnut #3//
+	fill(230, 236, 240);
+	ellipse(50, 50, 200, 200);
+	fill(0);
+	ellipse(50, 50, 130, 130);
+
+	drawDonut1(247, 8, 40, 150, 150);
+	drawDonut1(212, 239, 231, 670, 670);
+	drawDonut1(149, 80, 75, 780, 100);
+}
+
+//doughnut #1//
+function drawDonut1(r, g, b, x, y){
+	fill(r, g, b);
+	ellipse(x, y, 200, 200);
+	fill(0);
+	ellipse(x, y, 130, 130);
+}
